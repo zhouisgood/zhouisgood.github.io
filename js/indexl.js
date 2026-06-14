a940fe1dfda73a80e37218ab6f6fb1bbc1b03ace
zhouisgood <155302850+zhouisgood@users.noreply.github.com>
2026-06-15 06:53:34 +0800

    Update indexl.js

--- js/indexl.js
+++ js/indexl.js
@@ -18,7 +18,7 @@ var S = {
             m++;
       S.Shape.render();
       //console.log(m);
-      if(m==3500){
+      if(m==2500){
         window.location.href="../html/BirthdayCake.html";
       }
     });
